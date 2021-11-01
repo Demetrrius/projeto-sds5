fc7029eb39c86e9e2b5cb5d101d863d4c32cca64
Demetrius Tobias <demein@gmail.com>
2021-11-01 19:17:05 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div >
-      <h1>Bem vindo a semana do Spring React!</h1>
+      <h1 className="text-primary">Bem vindo a semana do Spring React!</h1>
     </div>
   );
 }
