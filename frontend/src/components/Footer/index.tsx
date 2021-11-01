b0fb17785c460886392376b7ae0ea2321e81a9bf
Demetrius Tobias <demein@gmail.com>
2021-11-01 20:49:25 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+       
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">GitHub <a href="https://github.com/Demetrrius" target="_blank" rel="noreferrer">Demetrius Tobias</a></p>
+                    <p className="text-light">Linkedin <a href="https://www.linkedin.com/in/demetriustobias/" target="_blank" rel="noreferrer">Demetrius Tobias</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        
+    );
+}
+
+export default Footer;
